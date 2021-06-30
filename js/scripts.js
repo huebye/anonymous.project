67907e301cccad2afe060c800557e21abe5ed4a2
huebye <thomashuebner94@gmx.de>
2021-06-30 10:32:25 +0200

    added artist collection view
    you can view the artists paintings now
    basic structure is being prepared for the backend

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,7 @@ let anonymousCollection = (function(){
                     {title: "Unhappy", artist: "Olaf"},
                     {title: "Holy", artist: "Harry"},
                     {title: "Unhappy", artist: "Chris"},
-                    {title: "Holy", artist: "Olga"},
+                    {title: "Holy", title2:  "Ambience", artist: "Olga"},
                     {title: "Unhappy", artist: "Hans"},
                     {title: "Holy", artist: "Helwetika"},
                     {title: "Unhappy", artist: "Thomas"},
@@ -44,7 +44,8 @@ let anonymousCollection = (function(){
                 let artList = document.querySelector('#artList');
                 let listItem = document.createElement('li');
                 let a = document.createElement('a');
-                a.innerText = art.artist.toUpperCase();
+                a.innerText = art.artist;
+                a.href = "#";
                 artList.appendChild(listItem);
                 listItem.appendChild(a);
                 a.addEventListener('click', function(){
@@ -54,14 +55,24 @@ let anonymousCollection = (function(){
 
               function artistCollectionProfile(art) {
                 let artCollection = document.querySelector('.artistCollection');
-                artCollection.style.display = "grid"
-                let title = document.createElement('p');
-                let image = document.createElement('img');
+                artCollection.style.display = "block";
+                let title = document.createElement('h1');
+                let title2 = document.createElement('p');
+                let image1 = document.createElement('img');
+                let image2 = document.createElement('img');
+                let image3 = document.createElement('img');
                 title.style.zIndex = "4"
-                title.innerText = art.title;
-                image.src = "https://picsum.photos/400/600?random=2"
+                title.innerText = art.artist;
+                title2.style.zIndex = "4"
+                title2.innerText = art.title;
+                image1.src = "https://picsum.photos/400/600?random=2";
+                image2.src = "https://picsum.photos/500/600?random=2";
+                image3.src = "https://picsum.photos/600/600?random=2";
                 artCollection.appendChild(title);
-                artCollection.appendChild(image);
+                artCollection.appendChild(image1);
+                artCollection.appendChild(title2);
+                artCollection.appendChild(image2);
+                artCollection.appendChild(image3);
               }
 
               return {
